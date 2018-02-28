6e30ae9e86a55faef819ba2b7232ceedc6191c74
luca.campli <campli.luca@gmail.com>
2018-02-28 11:15:11 +0530

    refactor store code for improved readability

--- src/store.js
+++ src/store.js
@@ -7,21 +7,15 @@ const sagaMiddleware = createSagaMiddleware();
 const initialState = {};
 const enhancers = [];
 const middleware = [sagaMiddleware];
-let composedEnhancers;
 
-if (
+const composeEnhancers =
   process.env.NODE_ENV === "development" &&
-  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-) {
-  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
-
-  composedEnhancers = composeEnhancers(
-    applyMiddleware(...middleware),
-    ...enhancers
-  );
-} else {
-  composedEnhancers = compose(applyMiddleware(...middleware), ...enhancers);
-}
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
+const composeFn = composeEnhancers || compose;
+const composedEnhancers = composeFn(
+  applyMiddleware(...middleware),
+  ...enhancers
+);
 
 const store = createStore(rootReducer, initialState, composedEnhancers);
 
