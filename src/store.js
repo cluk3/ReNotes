a13635f03a349fe4ec5c188ce4279a3599f70fc5
luca.campli <campli.luca@gmail.com>
2018-05-21 13:39:37 +0200

    add redux thunk

--- src/store.js
+++ src/store.js
@@ -2,11 +2,12 @@ import { createStore, applyMiddleware, compose } from "redux";
 import rootReducer from "./reducers";
 import createSagaMiddleware from "redux-saga";
 import rootSaga from "./sagas";
+import thunk from "redux-thunk";
 
 const sagaMiddleware = createSagaMiddleware();
 const initialState = {};
 const enhancers = [];
-const middleware = [sagaMiddleware];
+const middleware = [sagaMiddleware, thunk];
 
 const composeEnhancers =
   process.env.NODE_ENV === "development" &&
