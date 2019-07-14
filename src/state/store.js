2727d65fddeadc63d1940a5d2466efef7ee6fed4
luca.campli <campli.luca@gmail.com>
2019-07-14 19:25:40 -0300

    remove unused redux saga

--- src/state/store.js
+++ src/state/store.js
@@ -1,19 +1,12 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from './reducers';
-import createSagaMiddleware from 'redux-saga';
-import rootSaga from './sagas';
 import thunk from 'redux-thunk';
 import { createStateObserverMiddleware } from 'helpers/stateObserver';
 import notesObserver from 'Notes/modules/observer';
 
-const sagaMiddleware = createSagaMiddleware();
 const initialState = {};
 const enhancers = [];
-const middleware = [
-  sagaMiddleware,
-  thunk,
-  createStateObserverMiddleware(notesObserver)
-];
+const middleware = [thunk, createStateObserverMiddleware(notesObserver)];
 
 const composeEnhancers =
   process.env.NODE_ENV === 'development' &&
@@ -26,5 +19,4 @@ const composedEnhancers = composeFn(
 
 const store = createStore(rootReducer, initialState, composedEnhancers);
 
-sagaMiddleware.run(rootSaga);
 export default store;
